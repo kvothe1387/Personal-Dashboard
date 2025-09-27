63878bb664349a4129d60e0f73cd74a16fffbb6c
David McCullough <dmccullough488@gmail.com>
2025-09-27 00:18:31 -0500

    added .catch to set default background img on error

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     document.body.style.backgroundImage = `url(${data.urls.full})`
     document.getElementById('author').textContent = `By: ${data.user.name}`
   })
+  .catch(err => {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1657210489279-60a5a69d79df?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NTg5NDk2MTV8&ixlib=rb-4.1.0&q=80&w=1080)`
+  })
